397c4786e38b90e993154de1ce107c12234af1e2
Janine Luk <janineluk95@gmail.com>
2018-12-11 10:08:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+  	  get '/' do
+  	    erb :'views/index.erb'
+    	  resp.status = 200
+      end
 
+     resp.finish
+    end
 end
